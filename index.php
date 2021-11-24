82584c3cc1c77a0b5de5d5bf1e474c8cb28f00b0
unknown <gsimoes@gamingcampus.local>
2021-11-24 13:00:20 +0100

    base

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>jkl</title>
+  </head>
+  <body>
+    <form method="post" action="register.php">
+      <input type='email' name='email' />
+      <input type='password' name='password' />
+      <input type='text' name='login' />
+      <input type='submit' value='Me connecter' />
+    </form>
+  </body>
+</html>
