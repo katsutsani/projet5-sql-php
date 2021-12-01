030b0ad9e17f00dda59774157cdbb9706688d135
unknown <gsimoes@gamingcampus.local>
2021-12-01 19:35:19 +0100

    panel

--- js/script.js
+++ js/script.js
@@ -44,17 +44,6 @@ $(document).ready(function(){
   $('.dropdown-trigger').dropdown();
 });
 
-(function($) {
-		$(function() {
-
-$('.dropdown-button').dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      hover: true, // Activate on hover
-      belowOrigin: true, // Displays dropdown below the button
-      alignment: 'right' // Displays dropdown with edge aligned to the left of button
-    }
-  );
-
-		}); // End Document Ready
-})(jQuery); // End of jQuery name space
+function change(value){
+    document.getElementById("id-user").value=value;
+}
