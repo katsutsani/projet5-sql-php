ec47f27da03705c207065848b5403dbd07773086
unknown <gsimoes@gamingcampus.local>
2022-01-18 19:49:02 +0100

    sql panel

--- change_page.php
+++ change_page.php
@@ -1,127 +1,65 @@
 <?php
 require_once "config.php";
 
-$requete1 = 'SELECT p.*,a.* FROM pages p
-LEFT JOIN avis a ON p.id=a.page_id
-WHERE p.id ='.$_POST['id-page'].' ORDER BY `order`';
-$resultat = $pdo->prepare($requete1);
-$resultat->execute();
-$resultatPagesInfoAvis = $resultat->fetchAll(PDO::FETCH_ASSOC);
-
-$requete1 = 'SELECT p.*,o.* FROM pages p
-LEFT JOIN objectifs o ON p.id=o.page_id
-WHERE p.id ='.$_POST['id-page'].' ORDER BY `order`';
-$resultat = $pdo->prepare($requete1);
-$resultat->execute();
-$resultatPagesInfoObj = $resultat->fetchAll(PDO::FETCH_ASSOC);
-
-$requete1 = 'SELECT p.*,c.* FROM pages p
-LEFT JOIN carousel c ON p.id=c.page_id
-WHERE p.id ='.$_POST['id-page'].' ORDER BY `order`';
-$resultat = $pdo->prepare($requete1);
-$resultat->execute();
-$resultatPagesInfoCarousel = $resultat->fetchAll(PDO::FETCH_ASSOC);
-
-$requete1 = 'SELECT * FROM parallax where page_id ='.$_POST['id-page'].' AND `order` = 1';
-$resultat = $pdo->prepare($requete1);
-$resultat->execute();
-$resultatParallaxTop = current($resultat->fetchAll(PDO::FETCH_ASSOC));
-
-$requete1 = 'SELECT * FROM parallax where page_id ='.$_POST['id-page'].' AND `order` = 2';
-$resultat = $pdo->prepare($requete1);
-$resultat->execute();
-$resultatParallaxMid = current($resultat->fetchAll(PDO::FETCH_ASSOC));
-
-$requete1 = 'SELECT * FROM parallax where page_id ='.$_POST['id-page'].' AND `order` = 3';
-$resultat = $pdo->prepare($requete1);
-$resultat->execute();
-$resultatParallaxBot = current($resultat->fetchAll(PDO::FETCH_ASSOC));
-
-
-
-$sql = "UPDATE pages
-SET title=:title,img_link=:img_link,description=:description,subtitle=:subtitle  WHERE id=:page_changed";
+$sql = "UPDATE pages SET title=:title,img_link=:img_link,description=:description,subtitle=:subtitle WHERE id=:id";
 $dataBinded=array(
-    ':page_changed'=> $_POST['id-page'],
+    ':title'=> $_POST['title'],
     ':img_link'=> $_POST['img_link'],
     ':description'=> $_POST['description'],
-    ':subtitle'=> $_POST['subtitle']
+    ':subtitle'=> $_POST['subtitle'],
+    ':id' => $_POST['id-page']
 );
 $pre = $pdo->prepare($sql);
 $pre->execute($dataBinded);
-
-foreach ($resultatPagesInfoCarousel as $resultatPagesInfo) {
-  $sql = "UPDATE carousel SET carousel_link=:carousel".$resultatPagesInfo['order']." WHERE page_id=:page_changed AND `order`=".$resultatPagesInfo['order']."";
+for ($i=1; $i < 4 ; $i++) {
+  $sql = "UPDATE objectifs SET title_objectifs=:titleObj".$i.",description_objectifs=:contentObj".$i.",logo_objectifs=:lienObj".$i.",typeLogo_objectifs=:typeLogoObj".$i." WHERE page_id=:id AND `order`=".$i."";
   $dataBinded=array(
-    ':page_changed'=> $_POST['id-page'],
-    ':carousel'.$resultatPagesInfo['order']=> $_POST['carousel'.$resultatPagesInfo['order']]
+      ':titleObj'.$i.''=> $_POST['titleObj'.$i.''],
+      ':contentObj'.$i.''=> $_POST['contentObj'.$i.''],
+      ':lienObj'.$i.''=> $_POST['lienObj'.$i.''],
+      ':typeLogoObj'.$i.''=> $_POST['typeLogoObj'.$i.''],
+      ':id' => $_POST['id-page']
   );
   $pre = $pdo->prepare($sql);
   $pre->execute($dataBinded);
 }
 
-foreach ($resultatPagesInfoObj as $resultatPagesInfo) {
-  $sql = "UPDATE objectifs
-  SET logo_objectifs=:lienObj".$resultatPagesInfo['order'].",
-  typeLogo_objectifs=:typeLogoObj".$resultatPagesInfo['order'].",
-  title_objectifs=:titleObj".$resultatPagesInfo['order'].",
-  description_objectifs=:contentObj".$resultatPagesInfo['order'].",
-  WHERE page_id=:page_changed AND `order`=".$resultatPagesInfo['order']."";
+for ($i=1; $i < 4 ; $i++) {
+  if (isset($_POST['titleAvis'.$i.''])) {
+    $sql = "UPDATE avis SET title_avis=:titleAvis".$i.",description_avis=:contentAvis".$i.",logo_avis=:lienAvis".$i.",typeLogo_avis=:typeLogoAvis".$i." WHERE page_id=:id AND `order`=".$i."";
+    $dataBinded=array(
+        ':titleAvis'.$i.''=> $_POST['titleAvis'.$i.''],
+        ':contentAvis'.$i.''=> $_POST['contentObj'.$i.''],
+        ':lienAvis'.$i.''=> $_POST['lienAvis'.$i.''],
+        ':typeLogoAvis'.$i.''=> $_POST['typeLogoAvis'.$i.''],
+        ':id' => $_POST['id-page']
+    );
+    $pre = $pdo->prepare($sql);
+    $pre->execute($dataBinded);
+  }
+  else{
+    break;
+  }
+}
+
+for ($i=1; $i < 5 ; $i++) {
+  $sql = "UPDATE carousel SET carousel_link=:carousel".$i." WHERE page_id=:id AND `order`=".$i."";
   $dataBinded=array(
-    ':page_changed'=> $_POST['id-page'],
-    ':lienObj'.$resultatPagesInfo['order']=> $_POST['lienObj'.$resultatPagesInfo['order']],
-    ':typeLogoObj'.$resultatPagesInfo['order']=> $_POST['typeLogoObj'.$resultatPagesInfo['order']],
-    ':titleObj'.$resultatPagesInfo['order']=> $_POST['titleObj'.$resultatPagesInfo['order']],
-    ':contentObj'.$resultatPagesInfo['order']=> $_POST['contentObj'.$resultatPagesInfo['order']]
+      ':carousel'.$i.''=> $_POST['carousel'.$i.''],
+      ':id' => $_POST['id-page']
   );
   $pre = $pdo->prepare($sql);
   $pre->execute($dataBinded);
 }
 
-foreach ($resultatPagesInfoAvis as $resultatAvisModal) {
-  $sql = "UPDATE objectifs
-  SET logo_avis=:lienAvis".$resultatAvisModal['order'].",
-  typeLogo_avis=:typeLogoAvis".$resultatAvisModal['order'].",
-  title_avis=:titleAvis".$resultatAvisModal['order'].",
-  description_avis=:descAvis".$resultatAvisModal['order'].",
-   WHERE page_id=:page_changed AND `order`=".$resultatAvisModal['order']."";
+for ($i=1; $i < 4 ; $i++) {
+  $sql = "UPDATE parallax SET parallax_link=:parallax".$i." WHERE page_id=:id AND `order`=".$i."";
   $dataBinded=array(
-    ':page_changed'=> $_POST['id-page'],
-    ':lienAvis'.$resultatAvisModal['order']=> $_POST['lienAvis'.$resultatAvisModal['order']],
-    ':titleAvis'.$resultatAvisModal['order']=> $_POST['titleAvis'.$resultatAvisModal['order']],
-    ':typeLogoAvis'.$resultatAvisModal['order']=> $_POST['typeLogoAvis'.$resultatAvisModal['order']],
-    ':descAvis'.$resultatAvisModal['order']=> $_POST['descAvis'.$resultatAvisModal['order']]
+      ':parallax'.$i.''=> $_POST['parallax'.$i.''],
+      ':id' => $_POST['id-page']
   );
   $pre = $pdo->prepare($sql);
   $pre->execute($dataBinded);
 }
-
-$sql = "UPDATE parallax SET parallax_link=:parallax1,
- WHERE page_id=:page_changed AND `order`=1";
-$dataBinded=array(
-  ':page_changed'=> $_POST['id-page'],
-  ':parallax1'=> $_POST['parallax1']
-);
-$pre = $pdo->prepare($sql);
-$pre->execute($dataBinded);
-
-$sql = "UPDATE parallax SET parallax_link=:parallax2,
- WHERE page_id=:page_changed AND `order`=2";
-$dataBinded=array(
-  ':page_changed'=> $_POST['id-page'],
-  ':parallax2'=> $_POST['parallax2']
-);
-$pre = $pdo->prepare($sql);
-$pre->execute($dataBinded);
-
-$sql = "UPDATE parallax SET parallax_link=:parallax3,
- WHERE page_id=:page_changed AND `order`=3";
-$dataBinded=array(
-  ':page_changed'=> $_POST['id-page'],
-  ':parallax3'=> $_POST['parallax3']
-);
-$pre = $pdo->prepare($sql);
-$pre->execute($dataBinded);
-
-header('Location:panel.php?page=pages');//on le redirige sur la page d'accueil du site !
+header('Location:panel.php?page=pages');
 ?>
