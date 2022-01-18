0d9450c9b2ae5cdae5174bdbf8f5fcf24eeaf944
unknown <gsimoes@gamingcampus.local>
2022-01-18 20:41:01 +0100

    correction

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 session_start();
 
 $pdo = new PDO(
-    'mysql:host=localhost;dbname=portfolio;',
-    'root',
-    '',
+    'mysql:host=mysql-portfoliogc.alwaysdata.net;dbname=portfoliogc_test;',
+    '249600',
+    'Samspeed6425',
     array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8')
 );
 $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
